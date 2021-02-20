4a5719b610104b8d38cc28c09b40c65e9a909f33
cagy <ml.cagy@seznam.cz>
2021-02-20 20:40:53 +0100

    základní porovnání

--- js/classes.js
+++ js/classes.js
@@ -1,36 +1,37 @@
 class Prisoner {
-	
+
 	constructor(index, algorithmName) {
-	  
+
 		this.score = 0;
 		this.name = 'prisoner' + index;
 		this.algorithmName = algorithmName;
 		this.algorithmMethod = window[algorithmName.toUpperCase()];
+		this.iterationResult = 0;
 		
 		// Map, where key is name of prisoner and value is history with this prisoner.
 		this.history = new Map();
 	}
-	
+
 	// Get count of cooperates / deflects of current person.
 	getTypeCount(prisonerName, cooperate) {
-		
+
 		var prisonerHistory = this.history.get(prisonerName);
-		
+
 		if (prisonerHistory == null) {
-			
+
 			return 0;
 		}
-		
+
 		var count = 0;
-		
+
 		for (let i = 0; i < prisonerHistory.length; i++) {
-			
+
 			if (prisonerHistory[i] == cooperate) {
-		
+
 				count++;
 			}
 		}
-		
+
 		return count;
 	}
 }
--- js/index.js
+++ js/index.js
@@ -1,96 +1,161 @@
-prepareAlgorithmsOptions();
+var tmpHistory1 = [ ];
+var tmpHistory2 = [ ];
 
-function prepareAlgorithmsOptions() {
+let table = document.getElementById("results");
 
-	var optionAlg1 = document.getElementById("alg1");
-	var optionAlg2 = document.getElementById("alg2");
-		
-	var algorithms = getAlgorithms();
-		
-	for (i = 0; i < algorithms.length; i++) {
-		
-		let option = document.createElement('option');
-		option.value = algorithms[i];
-		option.innerHTML = algorithms[i];
-			
-		optionAlg1.appendChild(option);
-		optionAlg2.appendChild(option.cloneNode(true));
-		
-		if (i == 0) {
-			
-			optionAlg1.value = algorithms[i];
-			optionAlg2.value = algorithms[i];
-		}
-	}
-	const urlParams = new URLSearchParams(window.location.search)
-	
-	optionAlg1.value = urlParams.get('alg1') || 'ALLC'
-	optionAlg2.value = urlParams.get('alg2') || 'ALLC'
+let tbody = table.getElementsByTagName('tbody')[0];
 
-	document.getElementById('iterations').value = parseInt(urlParams.get('iterations') || 1);
-}
+var iterations = 0;
 
-function getAlgorithms() {
-	
-	return [
-		"ALLC",
-		"ALLD",
-		"ALT",
-		"APP",
-		"CPAVG",
-		"GRIM",
-		"PAV",
-		"RAND",
-		"TFT",
-		"TFTT",
-		"TTFT"
-	];
-}
+var i = 0;
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+			document.getElementById("vizualize-btn").addEventListener('click', function(e) {
+					e.preventDefault();
+					prisonersDilemma();
+			})
+});
 
 function prisonersDilemma() {
+	hideTable();
 
-	console.clear();
-	
-	var prisoner1 = new Prisoner(0, document.getElementById("alg1").value);	
-	var prisoner2 = new Prisoner(1, document.getElementById("alg2").value);
-	
-	var iterations = document.getElementById("algIterations").value;
-	
-	var tmpHistory1 = [ ];
-	var tmpHistory2 = [ ];
+	var prisoner1 = new Prisoner(0, document.getElementById("strategy-1").value);
+	var prisoner2 = new Prisoner(1, document.getElementById("strategy-2").value);
 
+	i = 0;
+
+	iterations = document.getElementById("iterations").value;
 	
-	for (let i = 0; i < iterations; i++) {
-	
-		var statePrisoner1 = prisoner1.algorithmMethod(tmpHistory1, tmpHistory2);
-		var statePrisoner2 = prisoner2.algorithmMethod(tmpHistory2, tmpHistory1);
-		
-		// True - cooperate with police, betray the other prisoner.
-		// False - do not cooperate with police.
-		if (statePrisoner1 && statePrisoner2) {
-			
-			// If A and B each betray the other, each of them serves two years in prison.
-			prisoner1.score += 2;
-			prisoner2.score += 2;
-			
-		} else if (!statePrisoner1 && !statePrisoner2){
-			
-			// If A and B both remain silent, both of them will only serve one year in prison (on the lesser charge).
-			prisoner1.score += 1;
-			prisoner2.score += 1;		
-			
-		} else {
-				
-			// If A betrays B but B remains silent, A will be set free and B will serve three years in prison (and vice versa)			
-			prisoner1.score += statePrisoner1 ? 0 : 3;
-			prisoner2.score += statePrisoner2 ? 0 : 3;
+	prepareTable(prisoner1, prisoner2);
+
+	runGame(prisoner1, prisoner2);
+
+	showResults();
+}
+
+function prepareTable(prisoner1, prisoner2)
+{
+		document.getElementById("alg1-name").innerHTML = prisoner1.algorithmName;
+		document.getElementById("alg2-name").innerHTML = prisoner2.algorithmName;
+
+		tbody.innerHTML = "";
+}
+
+function updateTotalScore(prisoner1, prisoner2)
+{
+		document.getElementById("alg1-score").innerHTML = prisoner1.score;
+		document.getElementById("alg2-score").innerHTML = prisoner2.score;
+}
+
+
+
+function runGame(prisoner1, prisoner2)
+{
+		setTimeout(function() {
+
+				var statePrisoner1 = prisoner1.algorithmMethod(tmpHistory1, tmpHistory2);
+				var statePrisoner2 = prisoner2.algorithmMethod(tmpHistory2, tmpHistory1);
+
+				// True - cooperate with police, betray the other prisoner.
+				// False - do not cooperate with police.
+				if (statePrisoner1 && statePrisoner2) {
+
+					// If A and B each betray the other, each of them serves two years in prison.
+					prisoner1.iterationResult = 2;
+					prisoner2.iterationResult = 2;
+
+				} else if (!statePrisoner1 && !statePrisoner2){
+
+					// If A and B both remain silent, both of them will only serve one year in prison (on the lesser charge).
+					prisoner1.iterationResult = 1;
+					prisoner2.iterationResult = 1;
+
+				} else {
+
+					// If A betrays B but B remains silent, A will be set free and B will serve three years in prison (and vice versa)
+					prisoner1.iterationResult = statePrisoner1 ? 0 : 3;
+					prisoner2.iterationResult = statePrisoner2 ? 0 : 3;
+				}
+
+				prisoner1.score += prisoner1.iterationResult;
+				prisoner2.score += prisoner2.iterationResult;
+
+
+				tmpHistory1.push(statePrisoner1);
+				tmpHistory2.push(statePrisoner2);
+
+
+				var row = tbody.insertRow();
+				row.className = 'row-animation';
+
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				var cell3 = row.insertCell(2);
+
+				cell1.innerHTML = (i + 1);
+				cell2.innerHTML = getDataForPrisonerCell(prisoner1, statePrisoner1);
+				cell3.innerHTML = getDataForPrisonerCell(prisoner2, statePrisoner2);
+
+				setTimeout(function() {
+						row.classList.add("visible");
+				}, 50);
+
+
+				updateTotalScore(prisoner1, prisoner2);
+
+	    	i++;
+
+
+	    if (i < iterations) {           //  if the counter < 10, call the loop function
+	      runGame(prisoner1, prisoner2);             //  ..  again which will trigger another
+	    }                       //  ..  setTimeout()
+	  }, 100);
+}
+
+
+function getDataForPrisonerCell(prisoner, state)
+{
+		var data = document.createElement("span");
+
+		var img = document.createElement('img');
+		img.classList.add("result-img");
+
+		if (state) {
+				img.src = "img/snitch.png"
 		}
-		
-		tmpHistory1.push(statePrisoner1);
-		tmpHistory2.push(statePrisoner2);
-		
-		console.log("\tIteration: " + (i + 1));
-		console.log("\t\tPrisoner 1: " + prisoner1.score);
-		console.log("\t\tPrisoner 2: " + prisoner2.score);
-	}
+		else {
+				img.src = "img/bro.png"
+		}
+
+		var totalScore = document.createElement("text");
+		totalScore.innerHTML = prisoner.score - prisoner.iterationResult;
+
+		var iterationResult = document.createElement("text");
+		iterationResult.innerHTML = "+ " + prisoner.iterationResult;
+
+		data.appendChild(totalScore);
+		data.appendChild(img);
+		data.appendChild(iterationResult);
+
+		return data.innerHTML;
+}
+
+function showResults()
+{
+		let table = document.getElementById("game-result");
+
+		if (table) {
+				table.classList.remove("hidden");
+		}
+}
+
+function hideTable()
+{
+		let table = document.getElementById("game-result");
+
+		if (table) {
+				table.classList.add("hidden");
+		}
 }
